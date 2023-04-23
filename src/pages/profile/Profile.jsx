d2706fe7cd66929b863d3c5cdfeee647ebcb7db4
Cherilyn Eugenia <cherilyn.eugenia@gmail.com>
2023-04-23 17:11:45 +0700

    connect backend profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Header from "../../components/Header"
 import { Card, CardBody, Button } from "@chakra-ui/react"
+import { http } from "../../utils"
 
 function ProfilePage(){
     const navigate = useNavigate()
@@ -9,13 +10,21 @@ function ProfilePage(){
     const [user, setUser] = useState({
         nama: 'Mikhael Chris',
         divisi: 'Admin',
-        jabatan: 'Staff Admin',
         nik: '12345',
         email: 'mikhaelc@gmail.com',
-        telp: '08123456789',
+        no_telp: '08123456789',
         alamat: 'Jalan Ngagel Jaya Tengah, Surabaya',
-        tglLahir: '1 Januari 1999'
+        tanggal_lahir: '1 Januari 1999'
     })
+
+    const fetchKaryawan = async () => {
+        const res = await http.get('/profile')
+        setUser(res.data.karyawan)
+    }
+
+    // useEffect(() => {
+    //     fetchKaryawan()
+    // }, [])
 
     return(
         <div className="w-screen h-full min-h-screen bg-gray">
@@ -24,46 +33,43 @@ function ProfilePage(){
                 <Card>
                     <CardBody>
                         <table>
-                            <tr>
-                                <td className="py-2 pr-2">Nama</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.nama}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">Divisi</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.divisi}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">Jabatan</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.jabatan}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">NIK</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.nik}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">Email</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.email}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">No. Telepon</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.telp}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">Alamat</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.alamat}</td>
-                            </tr>
-                            <tr>
-                                <td className="py-2">Tanggal Lahir</td>
-                                <td className="p-2">:</td>
-                                <td className="p-2">{user.tglLahir}</td>
-                            </tr>
+                            <tbody>
+                                <tr>
+                                    <td className="py-2 pr-2">Nama</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.nama}</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2">Divisi</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.divisi}</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2">NIK</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.nik}</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2">Email</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.email}</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2">No. Telepon</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.no_telp}</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2">Alamat</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.alamat}</td>
+                                </tr>
+                                <tr>
+                                    <td className="py-2">Tanggal Lahir</td>
+                                    <td className="p-2">:</td>
+                                    <td className="p-2">{user.tanggal_lahir}</td>
+                                </tr>
+                            </tbody>
                         </table>
                     </CardBody>
                 </Card>
--- src/pages/profile/UbahPass.jsx
+++ src/pages/profile/UbahPass.jsx
@@ -7,36 +7,61 @@ import {
 
 import Header from "../../components/Header"
 import InputPassword from "../../components/InputPassword"
+import { http } from "../../utils"
 
 function UbahPass(){
     const navigate = useNavigate()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const form = e.target
+        const formData = new FormData(form)
+
+        const data = {
+            oldpass: formData.get('oldpass'),
+            newpass: formData.get('newpass'),
+            confpass: formData.get('confpass'),
+        }
+        console.log(data)
+
+        navigate('/profil')
+
+        const res = await http.post('/profile/change-password', data)
+        console.log(res.data)
+
+        // if(res.status==200){
+        //     navigate('/profil')
+        // }
+    }
 
     return(
         <div className="w-screen h-full min-h-screen bg-gray">
             <Header user='MIKHAEL CHRIS' title='Ubah Password' subtitle='' />
             <div className="content p-10 text-left flex flex-col">
-                <FormControl className='mb-2'>
-                    <FormLabel>Password Lama</FormLabel>
-                    <div className="bg-white rounded-lg">
-                        <InputPassword />
+                <form method="post" onSubmit={handleSubmit}>
+                    <FormControl className='mb-2'>
+                        <FormLabel>Password Lama</FormLabel>
+                        <div className="bg-white rounded-lg">
+                            <InputPassword name={'oldpass'} />
+                        </div>
+                    </FormControl>
+                    <FormControl className='mb-2'>
+                        <FormLabel>Password Baru</FormLabel>
+                        <div className="bg-white rounded-lg">
+                            <InputPassword name={'newpass'} />
+                        </div>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Konfirmasi Password Baru</FormLabel>
+                        <div className="bg-white rounded-lg">
+                            <InputPassword name={'confpass'} />
+                        </div>
+                    </FormControl>
+                    <div className="buttons flex gap-x-2 mt-5">
+                        <Button colorScheme="primary" variant='outline' className="w-1/2" onClick={ ()=>{navigate('/profil')} } >Batal</Button>
+                        <Button type="submit" colorScheme="primary" variant='solid' className="bg-primary w-1/2">Ubah</Button>
                     </div>
-                </FormControl>
-                <FormControl className='mb-2'>
-                    <FormLabel>Password Baru</FormLabel>
-                    <div className="bg-white rounded-lg">
-                        <InputPassword />
-                    </div>
-                </FormControl>
-                <FormControl>
-                    <FormLabel>Konfirmasi Password Baru</FormLabel>
-                    <div className="bg-white rounded-lg">
-                        <InputPassword />
-                    </div>
-                </FormControl>
-                <div className="buttons flex gap-x-2 mt-5">
-                    <Button colorScheme="primary" variant='outline' className="w-1/2" onClick={ ()=>{navigate('/profil')} } >Batal</Button>
-                    <Button colorScheme="primary" variant='solid' className="bg-primary w-1/2">Ubah</Button>
-                </div>
+                </form>
             </div>
         </div>
     )
