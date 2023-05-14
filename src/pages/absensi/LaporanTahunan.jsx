300770fed822d5aed9bec8dead8122d3cb8646aa
Cherilyn Eugenia <cherilyn.eugenia@gmail.com>
2023-05-14 17:52:44 +0700

    add chart kehadiran, delete horizon components

--- src/pages/absensi/LaporanTahunan.jsx
+++ src/pages/absensi/LaporanTahunan.jsx
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react"
 import { Card, Select } from '@chakra-ui/react'
-
+import {
+    Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement,
+    Title, Tooltip, Filler, Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import Header from "../../components/Header"
+import { http } from '../../utils'
 
 function LaporanTahunan(){
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [bulan, setBulan] = useState(['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'])
     const [tahun, setTahun] = useState([2020, 2021, 2022, 2023, 2024, 2025])
+
+    const date = new Date()
+    const [selectedBulan, setSelectedBulan] = useState(date.getMonth()+1)
+    const [selectedTahun, setSelectedTahun] = useState(date.getFullYear())
+
+    const [laporan, setLaporan] = useState([])
+    const [labels, setLabels] = useState([])
+
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Filler,
+        Legend
+    )
+
+    const fetchLaporan = async () => {
+        const res = await http.get(`/absensi/laporan/${selectedTahun}/${selectedBulan}`)
+        setLaporan(res.data.laporan)
+        setLabels(res.data.labels)
+    }
 
     useEffect(() => {
         if(JSON.parse(localStorage.getItem('user'))==null){
@@ -13,21 +43,28 @@ function LaporanTahunan(){
         }
         else{
             setIsLoggedIn(true)
+            fetchLaporan()
+            console.log(laporan.length)
         }
-    })
+    }, [])
 
     return(
         <div className="w-screen h-full min-h-screen bg-gray">
             {
                 isLoggedIn &&
-                <Header title='Laporan Absensi Tahunan' subtitle='' />
+                <Header title='Grafik Kehadiran' subtitle='' />
             }
             <div className="content p-10 text-left flex flex-col">
                 <div className="flex gap-x-3">
                     <div className="bg-white rounded-lg w-2/3">
                         <Select>
-                            <option value='1'>Januari - Juni</option>
-                            <option value='1'>Juli - Desember</option>
+                            {
+                                bulan.map((b, index)=>{
+                                    return(
+                                        <option value={index+1} key={index} selected={index+1==selectedBulan}>{b}</option>
+                                    )
+                                })
+                            }
                         </Select>
                     </div>
                     <div className="bg-white rounded-lg w-1/3">
@@ -35,15 +72,53 @@ function LaporanTahunan(){
                             {
                                 tahun.map((t, index)=>{
                                     return(
-                                        <option value={t} key={index}>{t}</option>
+                                        <option value={t} key={index} selected={t==selectedTahun}>{t}</option>
                                     )
                                 })
                             }
                         </Select>
                     </div>
                 </div>
-                <Card>
-                    
+                <Card className="mt-10 p-10 w-full h-96">
+                    {
+                        laporan && (
+                        <Line 
+                            datasetIdKey="id" 
+                            options={{
+                                responsive: true,
+                                maintainAspectRatio: false,
+                                plugins: {
+                                    legend: {
+                                        position: 'top',
+                                    },
+                                    title: {
+                                        display: true,
+                                        text: `Grafik Kehadiran ${bulan[selectedBulan-1]}`,
+                                    },
+                                },
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }]
+                                }
+                            }} 
+                            data={{
+                                // labels: laporan.map((l)=>{return('')}),
+                                labels: labels,
+                                datasets: [{
+                                    fill: true,
+                                    label: 'Persentase Kehadiran',
+                                    data: laporan,
+                                    lineTension: 0.4,
+                                    borderColor: 'rgb(53, 162, 235)',
+                                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                                    borderWidth: 1
+                                }],
+                            }} 
+                        />)
+                    }
                 </Card>
             </div>
         </div>
