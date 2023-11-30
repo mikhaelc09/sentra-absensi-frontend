29d047bc51ed3a719ecee707138eadd3bc7b1d8b
Cherilyn Eugenia <cherilyn.eugenia@gmail.com>
2023-11-30 09:37:24 +0700

    edit axios headers

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,7 @@ export const http = axios.create({
     baseURL: import.meta.env.VITE_BACKEND ?? "http://localhost:3000/api/v1",
     headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested, Content-Type, Accept Authorization',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
         'x-auth-token': Cookies.get('token')
